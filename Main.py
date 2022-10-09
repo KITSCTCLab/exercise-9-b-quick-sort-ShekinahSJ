1f7edb965067913620e67b8a2919a570f9c4cf8e
ShekinahSJ <100009660+ShekinahSJ@users.noreply.github.com>
2022-10-09 22:58:19 +0530

    Initial Commit

--- Main.py
+++ Main.py
@@ -2,6 +2,15 @@ from typing import List
 
 def quick_sort(data, low, high) -> List[int]:
     # Write code here
+    i=low-1
+    pivot = data[high]
+    for j in range(low,high):
+        if data[j] <= pivot:
+            i+=1
+            data[i],data[j] = data[j],data[i]
+    data[i+1],data[high] = data[high],data[i+1]
+    return i+1
+
 
 
 input_data = input()
