b90edef308b0004b3ec901cbd7f3ab4651131079
ShekinahSJ <100009660+ShekinahSJ@users.noreply.github.com>
2022-10-09 23:00:10 +0530

    Third Commit

--- Main.py
+++ Main.py
@@ -9,7 +9,11 @@ def quick_sort(data, low, high) -> List[int]:
             i+=1
             data[i],data[j] = data[j],data[i]
     data[i+1],data[high] = data[high],data[i+1]
-    return i+1
+    j= i+1
+    if low<high:
+        quick_sort(data,low,j-1)
+        quick_sort(data,j+1,high)
+        return data
 
 
 
